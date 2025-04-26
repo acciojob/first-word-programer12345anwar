9e3ff43fd0239ad5df1ea5e61051570602ae1a75
Md Anwar Alam <113902153+programer12345anwar@users.noreply.github.com>
2025-04-26 16:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	//remove extra white spaces
+	s=s.trim();
+	//find first white space index
+	int index=s.indexOf(' ');
+	//if there is no white spaces then either it would be null or one word in this case return s
+	if(index==-1){
+		return s;
+	}
+	return s.substring(0,index);//0 to index-1
+	
 }
 
 // Do not change the code below
